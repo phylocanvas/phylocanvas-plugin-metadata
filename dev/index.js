9df23bc86ac1c240ebf9219ee8e8803b2a9ee3dd
KAD <kad0xf@gmail.com>
2016-05-03 17:52:31 +0100

    Generate random metadata columns on Dev page

--- dev/index.js
+++ dev/index.js
@@ -3,7 +3,15 @@ import metadataPlugin from '../src/index';
 
 PhyloCanvas.plugin(metadataPlugin);
 
-const tree = PhyloCanvas.createTree('phylocanvas');
+function getRandomColor() {
+  return '#'+Math.random().toString(16).slice(-6);
+}
+
+const numberOfColumns = 8; // parseInt(Math.random() * 10) + 1;
+
+const tree = PhyloCanvas.createTree('phylocanvas', {
+  padding: 1,
+});
 
 tree.on('error', function (event) { throw event.error; });
 
@@ -11,10 +19,23 @@ tree.on('loaded', function () {
   console.log('loaded');
   for (let i = 0; i <= 12; i++) {
     if (tree.leaves[i]) {
-      tree.leaves[i].data = { col: 1, x: 0, a: 1, c: 0 };
+      tree.leaves[i].data = {};
+      for (let j = 0; j < numberOfColumns; j++) {
+        tree.leaves[i].data[`col_${j}`] = getRandomColor();
+      }
     }
   }
   tree.viewMetadataColumns();
 });
+
+// tree.alignLabels = true;
+tree.setTreeType('rectangular');
+
+const originalDraw = tree.draw;
+tree.draw = (...args) => {
+  originalDraw.apply(tree, args);
+  const bounds = tree.getBounds();
+  tree.canvas.strokeRect(bounds[0][0], bounds[0][1], bounds[1][0] - bounds[0][0], bounds[1][1] - bounds[0][1]);
+};
 
 tree.load('(A:0.1,B:0.1,(C:0.1,D:0.1):0.1);');
